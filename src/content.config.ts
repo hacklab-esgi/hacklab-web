08f841ace8889a169222db2df93cbed8032fb20d
RemboFW <glavigne@myges.fr>
2025-04-20 16:00:38 +0200

    UPDATE info

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,32 +1,17 @@
 import { file, glob } from "astro/loaders";
 import { defineCollection, z, reference } from "astro:content";
 import type { icons as lucideIcons } from '@iconify-json/lucide/icons.json';
-import type { icons as simpleIcons } from '@iconify-json/simple-icons/icons.json';
 
 const lucideIconSchema = z.object({
   type: z.literal("lucide"),
   name: z.custom<keyof typeof lucideIcons>(),
 });
 
-const simpleIconSchema = z.object({
-  type: z.literal("simple-icons"),
-  name: z.custom<keyof typeof simpleIcons>(),
-});
-
-const quickInfo = defineCollection({
-  loader: file("src/content/info.json"),
-  schema: z.object({
-    id: z.number(),
-    icon: z.union([lucideIconSchema, simpleIconSchema]),
-    text: z.string(),
-  })
-});
-
 const socials = defineCollection({
   loader: file("src/content/socials.json"),
   schema: z.object({
     id: z.number(),
-    icon: z.union([lucideIconSchema, simpleIconSchema]),
+    icon: lucideIconSchema,
     text: z.string(),
     link: z.string().url(),
   })
@@ -65,11 +50,11 @@ const projects = defineCollection({
     info: z.array(
       z.object({
         text: z.string(),
-        icon: z.union([lucideIconSchema, simpleIconSchema]),
+        icon: lucideIconSchema,
         link: z.string().url().optional(),
       })
     )
   })
 });
 
-export const collections = { tags, posts, projects, quickInfo, socials };
+export const collections = { tags, posts, projects, socials };
